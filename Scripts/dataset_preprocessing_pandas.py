f80015e1e51a7f3a8584404e12f5861a9dd7fa96
PolMix <112618861+PolMix@users.noreply.github.com>
2023-05-14 20:15:57 +0300

    Upload N4

--- Scripts/dataset_preprocessing_pandas.py
+++ Scripts/dataset_preprocessing_pandas.py
@@ -4,13 +4,15 @@ import pandas as pd
 def split_df(df, general_param_number):
     """
     Splits full dataframe, containing both X and Y data, into two separate DataFrames of X and Y parts.
+    
+    Note that general parameters should ho first in df.
 
     Parameters
     ----------
     - df : pd.DataFrame
-        Dataframe to be split into X and Y parts. This dataframe should contain correct column names.
+        Dataframe to be split into X and Y parts.
     - general_param_number : int
-        Number of parameters which are general for all resonant modes. They should go first in df.columns.
+        Number of parameters which are general for all resonant modes.
 
     Returns
     ----------
